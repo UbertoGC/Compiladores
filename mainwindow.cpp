0aa90b9bc18d9740ce942866b884c9c3eaa7258b
dtamotu <104528081+dtamotu@users.noreply.github.com>
2022-08-24 10:39:46 -0500

    incluyendoCuadrado

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
-
+Mat image;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -19,16 +19,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_loadimg_clicked()
 {
-
-    Mat image, b, g, r,energia;
+    //Mat image, b, g, r,energia;
     Mat canales[3];
     image = imread("C:\\tf\\original.jpg");
     split(image, canales);
-    b = canales[0], g = canales[1], r = canales[2];
     imshow("aqp_plaza", image);
     waitKey(0);
 }
-
+void MainWindow::on_cua_clicked()
+{
+    Mat canales[3];
+    Mat imagen = imread("../original.jpg");
+    Point p1(30, 30);
+    Point p2(255, 255);
+    int thickness = 2;
+    rectangle(imagen, p1, p2,
+              Scalar(255, 0, 0),
+              thickness, LINE_4);
+    imshow("cuadrado", imagen);
+    waitKey(0);
+}
